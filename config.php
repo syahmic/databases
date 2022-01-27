869d276c9f1b2e68ff3ac8b64c0e9cfcabb92ab2
syahmic <95634450+syahmic@users.noreply.github.com>
2022-01-28 05:58:23 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ims506";
+ 
+/* Attempt to connect to MySQL database */
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn2 = new mysqli($servername, $username, $password, $dbname); 
+ 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($conn2->connect_error) {
+    die("Connection failed: " . $conn2->connect_error);
+}
+?>

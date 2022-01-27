0d74839e499288262b02f8210eccf4f17a3be0b6
syahmic <95634450+syahmic@users.noreply.github.com>
2022-01-28 05:51:54 +0800

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+
+<?php
+echo "
+<a href='index.php' target='_self'>Rumah</a>
+<a href='list.php' target='_self'>Senarai</a>
+<a href='list2.php' target='_self'>Senarai 2 Table</a>
+<a href='add.php' target='_self'>Tambah</a>
+<a href='delete.php' target='_self'>Padam</a>
+<a href='update.php' target='_self'>Kemaskini</a>
+";
+?>
+<br><br><br>
+</body>
+</html>

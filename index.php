043ab10eb655becf4b7fed3970e15171d5c658a8
syahmic <95634450+syahmic@users.noreply.github.com>
2022-01-28 05:50:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Add Records in Database</title>
+	<title>Basic CRUD</title>
 </head>
 <body>
 
 <?php
-include "connection.php"; // Using database connection file here
+include 'menu.php'; //include menu list from menu.php, you may edit accordingly
 
-if(isset($_POST['submit']))
-{		
-    $fullname = $_POST['fullname'];
-    $age = $_POST['age'];
+echo "<br><br>
+	<h1> Selamat datang</h1>
+	";
 
-    $insert = mysqli_query($db,"INSERT INTO `tblemp`(`fullname`, `age`) VALUES ('$fullname','$age')");
 
-    if(!$insert)
-    {
-        echo mysqli_error();
-    }
-    else
-    {
-        echo "Records added successfully.";
-    }
-}
-
-mysqli_close($db); // Close connection
+include 'footer.php';
 ?>
-
-<h3>Fill the Form</h3>
-
-<form method="POST">
-  Full Name : <input type="text" name="fullname" placeholder="Enter Full Name" Required>
-  <br/>
-  Age : <input type="text" name="age" placeholder="Enter Age" Required>
-  <br/>
-  <input type="submit" name="submit" value="Submit">
-</form>
 
 </body>
 </html>
